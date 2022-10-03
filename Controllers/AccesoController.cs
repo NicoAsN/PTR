d7030e79bc2f9f303665983242bf95970efbb392
Nnava <Nnava@DESKTOP-R04AR4R>
2022-10-02 21:29:42 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+using PTR.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PTR.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        static string cadena = "Data Source=(local);Initial Catalog=DB_PTR;Integrated Security=true";
+
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Usuario oUsuario)
+        {
+            bool registrado;
+            string mensaje;
+
+            if(oUsuario.contrasenia == oUsuario.confirmar_contrasenia)
+            {
+                oUsuario.contrasenia = ConvertirSha256(oUsuario.contrasenia);
+
+            }
+            else
+            {
+                ViewData["mensaje"] = "Las contrasenias no coinciden";
+                return View();
+            }
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("pa_registrar_usuario", cn);
+                cmd.Parameters.AddWithValue("PrimerNombre", oUsuario.primer_nom);
+                cmd.Parameters.AddWithValue("SegundoNombre", oUsuario.segundo_nom);
+                cmd.Parameters.AddWithValue("PrimerApellido", oUsuario.primer_ape);
+                cmd.Parameters.AddWithValue("SegundoApellido", oUsuario.segundo_ape);
+                cmd.Parameters.AddWithValue("Telefono", oUsuario.telefono);
+                cmd.Parameters.AddWithValue("Rut", oUsuario.rut);
+                cmd.Parameters.AddWithValue("DV", oUsuario.DV);
+                cmd.Parameters.AddWithValue("Correo", oUsuario.correo);
+                cmd.Parameters.AddWithValue("Contrasenia", oUsuario.contrasenia);
+                cmd.Parameters.Add("registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("mensaje", SqlDbType.VarChar,100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                cmd.ExecuteNonQuery(); 
+
+                registrado = Convert.ToBoolean(cmd.Parameters["registrado"].Value);
+                mensaje = cmd.Parameters["mensaje"].Value.ToString();
+            }
+
+            ViewData["mensaje"] = mensaje;
+
+            if (registrado)
+            {
+                return RedirectToAction("Login", "Acceso");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario oUsuario)
+        {
+            oUsuario.contrasenia = ConvertirSha256(oUsuario.contrasenia);
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("pa_validar_usuario", cn);                
+                cmd.Parameters.AddWithValue("Correo", oUsuario.correo);
+                cmd.Parameters.AddWithValue("Contrasenia", oUsuario.contrasenia);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+
+                oUsuario.id_usuario = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            }
+
+            if(oUsuario.id_usuario != 0)
+            {
+                Session["usuario"] = oUsuario;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["mensaje"] = "Usuario no encontrad";
+                return View();
+            }
+
+        }
+
+        public static string ConvertirSha256(string texto)
+        {
+            //using System.Text;
+            //Usar la referencia dem "System.Security.Cryptography"
+
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach(byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+            return Sb.ToString();
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTR.Models
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public string primer_nom { get; set; }
+        public string segundo_nom { get; set; }
+        public string primer_ape { get; set; }
+        public string segundo_ape { get; set; }
+        public int telefono { get; set; }
+        public int rut { get; set; }
+        public string DV { get; set; }
+        public string correo { get; set; }
+        public string contrasenia { get; set; }
+        public string confirmar_contrasenia { get; set; }
+    }
+}
