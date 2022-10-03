719d2c5b17b12c45c3ec3f28207da6f963c5ec48
Nnava <Nnava@DESKTOP-R04AR4R>
2022-10-02 21:29:42 -0300

    se Agrego registrar

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PTR.Models;
+using PTR.Permisos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTR.Controllers
+{
+    [ValidarSesion]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+    }
+}
---
+++ Permisos/ValidarSesionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTR.Permisos
+{
+    public class ValidarSesionAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (HttpContext.Current.Session["Usuario"] == null)
+            {
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+
+    }
+}
